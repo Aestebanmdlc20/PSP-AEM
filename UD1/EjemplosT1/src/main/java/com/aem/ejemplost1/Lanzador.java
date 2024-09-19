3cdf7c9a4754f75a997164af810c9fced8b1a976
Aestebanmdlc20 <165804900+Aestebanmdlc20@users.noreply.github.com>
2024-09-19 11:08:37 +0200

    usando classpath

--- UD1/EjemplosT1/src/main/java/com/aem/ejemplost1/Lanzador.java
+++ UD1/EjemplosT1/src/main/java/com/aem/ejemplost1/Lanzador.java
@@ -20,46 +20,52 @@ public class Lanzador {
     
     public static void main(String[] args) throws IOException{
         Lanzador l = new Lanzador();
-        l.lanzarSumador(1, 51);
-        l.lanzarSumador(51, 100);
+        l.lanzarSumador(1, 51, "sumador1");
+        l.lanzarSumador(51, 100, "sumador2");
     }
     
-    public void lanzarSumador(int n1, int n2) throws IOException{
+    public void lanzarSumador(int n1, int n2, String fsalida) throws IOException{
        String clase="com.aem.ejemplost1.Sumador";
        ProcessBuilder pb;
        Process process = null;
+       int exitValue=0;
+       
        try {
-           pb = new ProcessBuilder("java", clase, String.valueOf(n1), String.valueOf(n2));
+           String classPath = ".;./target/classes"; //directorio donde tenemos las clases
+           
+           pb = new ProcessBuilder("java", "-cp", classPath, clase, String.valueOf(n1), String.valueOf(n2));
            
            //Con esto redirecciono el error
-           pb.redirectError(new File("Files" + File.separator + "error.log"));
+           pb.redirectError(new File("Files" + File.separator + "error_" + fsalida + System.currentTimeMillis()+".log"));
+           
+           //Escrbir datos generados por sumador en un fichero
+           pb.redirectOutput(new File("Files" + File.separator + fsalida+".log"));
            
            //Cambia el directorio de trabajo, al directorio donde se encuentran los .class
-           pb.directory(new File("target" + File.separator + "classes"));
+           //pb.directory(new File("target" + File.separator + "classes"));
            
-           process=pb.start();
+           process=pb.start(); //el proceso padre, espera a que el proceso hijo termine
+                     
+           exitValue = process.waitFor(); //Hago esperar a mi programa principal hasta que el process termine
            
-           //el proceso padre, espera a que el proceso hijo termine
-           //pb.directory(new File("bin")
-           
-           int exitValue = process.waitFor();
            System.out.println("Exit value: " + exitValue);
        } catch (Exception e) {
            e.printStackTrace();
        }
        
-       //Mostrar datos generados por Sumador
+       //Mostrar datos generados por Sumador en la consola
        
-       try (InputStream is = process.getInputStream()) {
-           int c;
-           while ((c = is.read()) != -1){
-               System.out.print((char) c);
-           }
-       } catch(IOException e){
-           e.printStackTrace();
-           throw e;
+       if(exitValue==0){
+           try (InputStream is = process.getInputStream()) {
+                int c;
+                while ((c = is.read()) != -1){
+                    System.out.print((char) c);
+                }
+            } catch(IOException e){
+                    e.printStackTrace();
+                    throw e;
+            }
        }
     }
- 
 }
 
