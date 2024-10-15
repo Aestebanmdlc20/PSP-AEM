bf5cdde6ac790fe2a8fcf4fa8cab9c9b7e100c97
Aestebanmdlc20 <165804900+Aestebanmdlc20@users.noreply.github.com>
2024-10-15 20:47:58 +0200

    .

---
+++ UD1/Ejercicio2/src/main/java/com/aem/ejercicio2/Ejercicio2.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.aem.ejercicio2;
+
+/**
+ *
+ * @author AEM by Alejandro Esteban Martinez de la Casa
+ */
+public class Ejercicio2 {
+
+    public static void main(String[] args) {
+        
+    }
+    
+    
+    
+}
